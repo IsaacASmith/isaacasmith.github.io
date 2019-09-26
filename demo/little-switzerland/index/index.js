1d794b51211d0b80b38930be87b5db1c3c91d8b0
Isaac Smith <isaac.smith.sf@gmail.com>
2019-09-25 19:43:28 -0600

    Added nav menu label

--- demo/little-switzerland/index/index.js
+++ demo/little-switzerland/index/index.js
@@ -1,11 +1,12 @@
-function showNav(){
-    document.getElementById('top-nav').style.display = 'flex';
-    document.getElementById('close-nav-btn').style.display = 'block';
-    document.getElementById('open-nav-btn').style.display = 'none';
-}
-
-function hideNav(){
-    document.getElementById('top-nav').style.display = 'none';
-    document.getElementById('close-nav-btn').style.display = 'none';
-    document.getElementById('open-nav-btn').style.display = 'block';
+function toggleNav(){
+    if(document.getElementById('top-nav').style.display == 'none') {
+        document.getElementById('top-nav').style.display = 'flex';
+        document.getElementById('close-nav-btn').style.display = 'block';
+        document.getElementById('open-nav-btn').style.display = 'none';
+    }
+    else {
+        document.getElementById('top-nav').style.display = 'none';
+        document.getElementById('close-nav-btn').style.display = 'none';
+        document.getElementById('open-nav-btn').style.display = 'block';
+    }
 }
