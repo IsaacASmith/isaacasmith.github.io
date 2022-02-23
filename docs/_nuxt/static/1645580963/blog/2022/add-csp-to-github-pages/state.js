9a9a143fde54ada23c131f5f5a481c39ea3bcb52
Isaac Smith <isaac.smith.sf@gmail.com>
2022-02-22 19:49:25 -0600

    Release 2022022201

---
+++ docs/_nuxt/static/1645580963/blog/2022/add-csp-to-github-pages/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645580963",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2022\u002Fadd-csp-to-github-pages",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
