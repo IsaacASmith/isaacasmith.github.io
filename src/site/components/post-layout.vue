2dd42199c9cd8d782adeb04f7e9461541d015d25
Isaac Smith <isaac.smith.sf@gmail.com>
2022-02-24 21:11:35 -0600

    Added github pages csp post

--- src/site/components/post-layout.vue
+++ src/site/components/post-layout.vue
@@ -1,20 +1,30 @@
 <template>
-  <div class="mx-3 md:mx-0">
-    <div class="flex flex-row items-center">
+  <div>
+    <div class="mx-3 md:mx-0 bg-white p-4 sm:p-8 rounded">
       <h1 class="text-3xl">{{ title }}</h1>
+      <div class="flex flex-row items-center mt-2 mb-6 flex-wrap">
+        <div class="mr-3 text-md text-gray-700">
+          {{ publishDate | formatMonthYear }}
+        </div>
+        <br />
+      </div>
+      <div class="flex flex-col items-center">
+        <img :src="thumbnail" class="pb-6" />
+      </div>
+      <div class="leading-8 text-lg font-normal">
+        <slot></slot>
+      </div>
     </div>
-    <div class="text-md mt-2 text-gray-700">
-      {{ publishDate | formatMonthYear }}
-    </div>
-    <slot></slot>
-    <div class="flex flex-row mt-3 text-xs items-center">
-      <span class="mr-2 text-sm">Tags: </span>
-      <div
-        v-for="tag in tags"
-        :key="tag"
-        class="px-2 py-1 bg-primary text-white mr-2 rounded"
-      >
-        {{ tag }}
+    <div class="px-8 mt-10">
+      <div class="text-xl font-bold mb-3">Tags</div>
+      <div class="flex flex-row items-center">
+        <div
+          v-for="tag in tags"
+          :key="tag"
+          class="px-2 py-1 bg-primary text-white mr-2 rounded text-xs"
+        >
+          {{ tag }}
+        </div>
       </div>
     </div>
   </div>
@@ -34,6 +44,10 @@ export default {
     tags: {
       type: Array,
       default: () => [],
+    },
+    thumbnail: {
+      type: String,
+      default: '',
     },
   },
 }
